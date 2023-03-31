1bac333caee4b4b2a53c8b6620180eb5802a8663
cr0cus <silvercrocus@proton.me>
2023-03-31 13:44:38 +0200

    create a copy of ls program(just for fun)

---
+++ ls.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+void _ls(const char *dir_name) {
+    DIR *dir;
+    struct dirent *entry;
+
+    if((dir = opendir(dir_name)) == NULL) {
+        printf("Couldn't open this directory");
+    }
+
+    else {
+        while((entry = readdir(dir)) != NULL) {
+            if((strcmp(entry->d_name, ".") == 0) || (strcmp(entry->d_name, "..") == 0)) continue;
+            printf("%s \n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+}
+
+
+int main(int args, char** argv) {
+    _ls(argv[1]);
+    return 0;
+}
