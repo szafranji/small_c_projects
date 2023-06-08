b7551d3a89217e3745a90a3782a6a90484b6df99
cr0cus <silvercrocus@proton.me>
2023-06-08 22:46:59 +0200

    finished cat.c

--- cat.c
+++ cat.c
@@ -3,11 +3,19 @@
 
 int main(int argc, char** argv) {
     FILE *file;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-    const char* file_name = argv;
+    const char *file_name = argv[1];
 
     if(!(file = fopen(file_name, "r"))) {
-        printf("Error");
+        printf("Error!");
         exit(1);
     }
+    while((read = getline(&line, &len, file)) != -1) {
+        printf("%s", line);
+    }
+
+    fclose(file);
 }
