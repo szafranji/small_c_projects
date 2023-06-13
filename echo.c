3abd62a3f04fe16e67a8c8515e7caf411253e1d0
cr0cus <silvercrocus@proton.me>
2023-06-13 23:39:27 +0200

    add some features

--- echo.c
+++ echo.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(int argc, char **argv) {
     if(argc == 1) printf("\n");
+    else if(argc > 2) str_output(argc, argv);
 
+}
+
+void str_output(int argc, char **argv) {
+    // use this function only for output 
+    // if we don't have > in argv
+
+    for(int i = 1; i < argc; i++) printf("%s ", argv[i]);
+
+    printf("\n");
+}
+
+
+int find_indexes(int argc, char **argv) {
+    for(int i = 1; i < argc-1; i++) {
+        if(argv[i] == '>') return i;
+    }
 }
 
 int find_arrow(int argc, char**argv) {
     for(int i = 1; i < argc-1; i++) {
-        if(argv[i] == '>') return 1;
+        if(argv[i] == '!') return 1;
         else return 0;
     }
 }
+
